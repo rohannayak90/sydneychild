c27b64cf7fca19334a62777a36a83c503374fbc2
rohannayak90 <rohannayak90@gmail.com>
2016-08-18 00:57:13 +0530

    message

--- dump/archive.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if( is_post_type_archive('jetpack-portfolio') )
-        get_template_part('content', 'jetpack-portfolio');
-
-    elseif( is_tax('jetpack-portfolio-type') || is_tax('jetpack-portfolio-tag') )
-        get_template_part('archive', 'jetpack-portfolio');
-
-    else continue;
-?>
