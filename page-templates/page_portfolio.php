776ecc81dd9b7820feddb0058d6ef54c3731db25
Murari <murari.nayak@bridgera.com>
2019-07-17 18:23:43 +0530

    Portfolio page cleanup

--- page-templates/page_portfolio.php
+++ page-templates/page_portfolio.php
@@ -1,36 +1,21 @@
 <?php
-
 /*
-
-Template Name: Portfolio Page - Child
-
+	Template Name: Portfolio Page - Child
 */
 	get_header();
 ?>
-
-
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php
-
-					if ( comments_open() || '0' != get_comments_number() ) :
-
-						comments_template();
-
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<?php
+			while (have_posts()):
+				the_post();
+				get_template_part( 'content', 'page' );
+				if ( comments_open() || '0' != get_comments_number() ) :
+					comments_template();
+				endif;
+			endwhile; // end of the loop. ?>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
 
