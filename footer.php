ba7ad52c67f52200ebff716195c73c4478b09e82
murarinayak <murari2nayak@gmail.com>
2021-04-04 11:35:53 +0530

    Updated GoogleAnalytics to GA4

--- footer.php
+++ footer.php
@@ -27,7 +27,7 @@
 <?php wp_footer(); ?>
 
 </body>
-<script>
+<!-- <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -36,5 +36,15 @@
   ga('create', 'UA-79155581-1', 'auto');
   ga('send', 'pageview');
 
+</script> -->
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-35EWEEBBQX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'G-35EWEEBBQX');
 </script>
+
 </html>
