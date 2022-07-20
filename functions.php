fc030f5d1825951e44cd4df01a333dc97408438e
rohannayak90 <rohannayak90@gmail.com>
2022-07-20 08:35:30 -0400

    Temp change

--- functions.php
+++ functions.php
@@ -1,10 +1,15 @@
 <?php
-	add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
-	function my_theme_enqueue_styles() {
-		wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-			array( 'parenthandle' ), 
-			wp_get_theme()->get('Version') // this only works if you have Version in the style header
-		);
+// 	add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+// 	function my_theme_enqueue_styles() {
+// 		wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+// 			array( 'parenthandle' ), 
+// 			wp_get_theme()->get('Version') // this only works if you have Version in the style header
+// 		);
+// 	}
+// 	
+	add_action( 'wp_enqueue_scripts', 'sydney_child_enqueue' );
+	function sydney_child_enqueue() {
+    	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	}
 
 	// add_filter('excerpt_more', 'get_read_more_link');
